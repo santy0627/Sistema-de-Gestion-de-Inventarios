3fda0b60470802fbfe305cd22d2cd764cb3048ec
santy0627 <santiagomoralestutistar123@gmail.com>
2026-07-09 22:27:08 -0500

    test: se definen los primeros 3 test iniciales

--- backend/tests/test_inventory.py
+++ backend/tests/test_inventory.py
@@ -5,19 +5,31 @@ from app.domain.value_objects.quantity import Quantity
 from app.domain.enums.currency import Currency
 from decimal import Decimal
 from app.domain.entities.inventory import Inventory
+from app.domain.exceptions.inventory_exceptions import ProductNotInInventoryError
 
-producto1 = Product("RTX 5070", "GPU-5070", "Nvidia", "GPU", Money(Decimal(5000000), Currency.COP), True)
-producto2 = Product("RTX 4070", "GPU-4070", "Nvidia", "GPU", Money(Decimal(4000000), Currency.COP), True)
-producto3 = Product("RTX 3050", "GPU-3050", "Nvidia", "GPU", Money(Decimal(3500000), Currency.COP), True)
+product1 = Product("RTX 5070", "GPU-5070", "Nvidia", "GPU", Money(Decimal(5000000), Currency.COP), True)
+product2 = Product("RTX 4070", "GPU-4070", "Nvidia", "GPU", Money(Decimal(4000000), Currency.COP), True)
+product3 = Product("RTX 3050", "GPU-3050", "Nvidia", "GPU", Money(Decimal(3500000), Currency.COP), True)
 
-producto4 = Product("RTX 4070", "GPU-4070", "Nvidia", "GPU", Money(Decimal(8000000), Currency.COP), True)
-
-item1 = InventoryItem(producto1, Quantity(10))
-item2 = InventoryItem(producto2, Quantity(20))
-item3 = InventoryItem(producto3, Quantity(30))
-
-inventory = Inventory(items=[item1,item2,item3])
+item1 = InventoryItem(product1, Quantity(10))
+item2 = InventoryItem(product2, Quantity(20))
+item3 = InventoryItem(product3, Quantity(30))
 
 def test_search_product():
-    result = inventory.search_product(producto4)
-    assert result.product.sku == producto4.sku and result.quantity == item2.quantity
+    inventory = Inventory(items=[item1,item2,item3])
+    product = Product("RTX 4070", "GPU-4070", "Nvidia", "GPU", Money(Decimal(8000000), Currency.COP), True)
+    result = inventory.search_product(product)
+    assert result.product.sku == product.sku and result.quantity == item2.quantity
+
+def test_add_stock():
+    inventory = Inventory(items=[item1,item2,item3])
+    inventory.add_stock(product2, Quantity(5))
+    assert inventory.stock(product2) == Quantity(25)
+
+def test_search_product_error():
+    inventory = Inventory(items=[item1,item2,item3])
+    product = Product("RTX 4070Ti", "GPU-4070Ti", "Nvidia", "GPU", Money(Decimal(8000000), Currency.COP), True)
+    try:
+        result = inventory.search_product(product)
+    except ProductNotInInventoryError as e:
+        assert str(e) == "El producto no existe en el inventario"
